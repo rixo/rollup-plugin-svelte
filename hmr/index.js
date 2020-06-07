126eae0478814ca46abef326daf8c49b546c72d2
rixo <rixo@rixo.fr>
2020-06-07 03:01:09 +0200

    fix options defaults

--- hmr/index.js
+++ hmr/index.js
@@ -12,11 +12,11 @@ const svelteHmr = (hotOptions = {}, pluginOptions = {}) => {
 	const { include, exclude } = pluginOptions;
 
 	const {
-		hot = false,
+		hot = true,
 		compatNollup = !!process.env.NOLLUP,
 		patchSapperDevClient = false,
 		test = process.env.NODE_ENV === 'test',
-	} = hotOptions === true ? { hot: true } : hotOptions;
+	} = hotOptions;
 
 	// careful with relative paths (see https://github.com/rixo/svelte-hmr/issues/11)
 	const hotApi = path.join(__dirname, 'runtime.js');
